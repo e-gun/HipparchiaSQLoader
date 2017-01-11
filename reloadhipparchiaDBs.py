05b8089b1a873d1d90dfbe0c833f3fa9cfd2aa6b
EG <egun@antisigma.classics.utoronto.ca>
2017-01-10 20:34:27 -0500

    drop all old tables and create new ones first; then reload

--- reloadhipparchiaDBs.py
+++ reloadhipparchiaDBs.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#!../bin/python
+# !../bin/python
 """
 	HipparchiaServer: an interface to a database of Greek and Latin texts
 	Copyright: E Gunderson 2016-17
@@ -15,16 +15,14 @@ from dbhelpers import *
 from multiprocessing import Process, Manager
 from mpclass import MPCounter
 
-
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-datadir = config['io']['datadir']+'sqldumps/'
-
+datadir = config['io']['datadir'] + 'sqldumps/'
 
 dbconnection = psycopg2.connect(user=config['db']['DBUSER'], host=config['db']['DBHOST'],
-                                port=config['db']['DBPORT'], database=config['db']['DBNAME'],
-                                password=config['db']['DBPASS'])
+								port=config['db']['DBPORT'], database=config['db']['DBNAME'],
+								password=config['db']['DBPASS'])
 
 # dbconnection.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
 cursor = dbconnection.cursor()
@@ -37,11 +35,11 @@ def retrievedb(location):
 	:param location:
 	:return:
 	"""
-	
+
 	f = gzip.open(location, 'rb')
 	dbcontents = pickle.load(f)
 	f.close()
-	
+
 	return dbcontents
 
 
@@ -52,34 +50,34 @@ def resetdb(dbname, structuremap, cursor):
 	:param cursor:
 	:return:
 	"""
-	
+
 	query = 'DROP TABLE IF EXISTS public.' + dbname
 	cursor.execute(query)
 	dbconnection.commit()
-	
+
 	query = 'CREATE TABLE public.' + dbname + ' ('
 	for column in structuremap:
 		if column[0] == 'index':
-			query += column[0]+' '+column[1]+' DEFAULT nextval(\'' + dbname + '\'::regclass), '
+			query += column[0] + ' ' + column[1] + ' DEFAULT nextval(\'' + dbname + '\'::regclass), '
 		else:
 			query += column[0] + ' ' + column[1] + ', '
 	query = query[:-2] + ') WITH ( OIDS=FALSE );'
 	cursor.execute(query)
 	dbconnection.commit()
-	
-	if re.search(r'(gr|lt|in|dp|ch)\d\d\d\d',dbname) is not None:
-		query = 'DROP INDEX IF EXISTS public.'+dbname+'_mu_trgm_idx'
+
+	if re.search(r'(gr|lt|in|dp|ch)\d\d\d\d', dbname) is not None:
+		query = 'DROP INDEX IF EXISTS public.' + dbname + '_mu_trgm_idx'
 		cursor.execute(query)
 		dbconnection.commit()
-		
-		query = 'DROP INDEX IF EXISTS public.'+dbname+'_st_trgm_idx'
+
+		query = 'DROP INDEX IF EXISTS public.' + dbname + '_st_trgm_idx'
 		cursor.execute(query)
 		dbconnection.commit()
-	
+
 	query = 'GRANT SELECT ON TABLE ' + dbname + ' TO hippa_rd;'
 	cursor.execute(query)
 	dbconnection.commit()
-	
+
 	return
 
 
@@ -98,13 +96,14 @@ def reloadwhoeldb(dbcontents, cursor):
 	for line in data:
 		count += 1
 		# 32k is the limit?
-		if count % 20000 == 0:
+		if count % 5000 == 0:
 			dbconnection.commit()
-			print('\t\tlongdb: ',dbname,'[ @ line ',count,']')
+		if count % 50000 == 0:
+			print('\t\tlongdb: ', dbname, '[ @ line ', count, ']')
 		reloadoneline(line, dbname, structure, cursor)
-	
+
 	dbconnection.commit()
-	
+
 	return
 
 
@@ -117,25 +116,25 @@ def reloadoneline(insertdata, dbname, dbstructurelist, cursor):
 	:param cursor:
 	:return:
 	"""
-	
+
 	insertstring = ' ('
 	insertvals = '('
 	for label in dbstructurelist:
 		insertstring += label[0] + ', '
-		insertvals	+= '%s, '
-	
+		insertvals += '%s, '
+
 	insertstring = insertstring[:-2] + ')'
 	insertvals = insertvals[:-2] + ')'
-	
+
 	q = 'INSERT INTO ' + dbname + insertstring + ' VALUES ' + insertvals
 	d = insertdata
-	
+
 	try:
-		cursor.execute(q,d)
+		cursor.execute(q, d)
 	except psycopg2.DatabaseError as e:
 		print('insert into ', dbname, 'failed at while attempting', d)
 		print('Error %s' % e)
-		
+
 	return
 
 
@@ -146,25 +145,25 @@ def buildfilesearchlist(datadir, memory):
 	:param datadir:
 	:return: a list of DirEntries
 	"""
-	
+
 	suffix = '.pickle.gz'
-	
+
 	for entry in os.scandir(datadir):
 		if entry.is_dir():
 			buildfilesearchlist(entry.path, memory)
 		elif suffix in entry.name:
 			memory.append(entry)
-		
+
 	entries = []
 	pickles = re.compile(r'\.pickle\.gz$')
 	for m in memory:
-		if re.search(pickles,m.path):
+		if re.search(pickles, m.path):
 			entries.append(m)
-	
+
 	paths = []
 	for e in entries:
 		paths.append(e.path)
-	
+
 	return paths
 
 
@@ -173,6 +172,10 @@ def recursivereload(datadir):
 	aim me at a directory and I will unpack all of the pickles and put them in the db
 	:return:
 	"""
+
+	dbc = setconnection(config)
+	cur = dbc.cursor()
+
 	structuremap = {
 		'authors': strauthors,
 		'works': strworks,
@@ -183,34 +186,45 @@ def recursivereload(datadir):
 		'greek_morphology': strgreek_morphology,
 		'latin_morphology': strlatin_morphology,
 		'builderversion': strbuilderversion
-		}
-	
+	}
+
 	print('scanning the filesystem')
-	dbpaths = buildfilesearchlist(datadir,[])
+	dbpaths = buildfilesearchlist(datadir, [])
 	totaldbs = len(dbpaths)
-	
-	print('beginning to reload the databases:',totaldbs,'found')
+
+	print('dropping any old tables and creating new ones')
+
+	authorfinder = re.compile(r'(gr|lt|in|dp|ch)\d\d\d\d$')
+	for db in dbpaths:
+		dbcontents = retrievedb(db)
+		if dbcontents['dbname'] in structuremap:
+			resetdb(dbcontents['dbname'], structuremap[dbcontents['dbname']], cur)
+		elif re.search(authorfinder, dbcontents['dbname']):
+			resetdb(dbcontents['dbname'], strindividual_authorfile, cur)
+	dbc.commit()
+
+	print('beginning to reload the databases:', totaldbs, 'found')
 	manager = Manager()
 	count = MPCounter()
 	dbs = manager.list(dbpaths)
 	workers = int(config['io']['workers'])
-	
-	jobs = [Process(target=mpreloader, args=(dbs, count, totaldbs, structuremap)) for i in
-	        range(workers)]
-	
+
+	jobs = [Process(target=mpreloader, args=(dbs, count, totaldbs)) for i in
+			range(workers)]
+
 	for j in jobs: j.start()
 	for j in jobs: j.join()
-	
+
 	return
 
 
-def mpreloader(dbs, count, totaldbs, structuremap):
+def mpreloader(dbs, count, totaldbs):
 	"""
 	mp reader reloader
 	:return:
 	"""
 	authorfinder = re.compile(r'(gr|lt|in|dp|ch)\d\d\d\d$')
-	
+
 	dbc = setconnection(config)
 	cur = dbc.cursor()
 
@@ -221,34 +235,32 @@ def mpreloader(dbs, count, totaldbs, structuremap):
 		except:
 			dbcontents = {}
 			dbcontents['dbname'] = ''
-		
+
 		count.increment()
-		if count.value % 250 == 0:
-			print('\t',str(count.value), 'of', str(totaldbs), 'databases restored')
-		
-		if dbcontents['dbname'] in structuremap:
-			resetdb(dbcontents['dbname'], structuremap[dbcontents['dbname']], cur)
-		elif re.search(authorfinder, dbcontents['dbname']):
-			resetdb(dbcontents['dbname'], strindividual_authorfile, cur)
-		
+		if count.value % 200 == 0:
+			print('\t', str(count.value), 'of', str(totaldbs), 'databases restored')
+
+		# print('loading',dbcontents['dbname'])
+
 		if dbcontents['dbname'] != '':
 			reloadwhoeldb(dbcontents, cur)
-		
+
 		if re.search(authorfinder, dbcontents['dbname']):
 			query = 'CREATE INDEX ' + dbcontents['dbname'] + '_mu_trgm_idx ON public.' + dbcontents['dbname'] + \
-			        ' USING gin (accented_line COLLATE pg_catalog."default" gin_trgm_ops)'
+					' USING gin (accented_line COLLATE pg_catalog."default" gin_trgm_ops)'
 			cur.execute(query)
 			dbc.commit()
-			
+
 			query = 'CREATE INDEX ' + dbcontents['dbname'] + '_st_trgm_idx ON public.' + dbcontents['dbname'] + \
-			        ' USING gin (stripped_line COLLATE pg_catalog."default" gin_trgm_ops)'
+					' USING gin (stripped_line COLLATE pg_catalog."default" gin_trgm_ops)'
 			cur.execute(query)
 			dbc.commit()
 
 	dbc.commit()
 	del dbc
-	
+
 	return
+
 
 # everything
 recursivereload(datadir)
